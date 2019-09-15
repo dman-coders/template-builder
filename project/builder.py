58362ff975757123aedb559b3db5d40d90a355ea
Christopher Skene <chris@xtfer.com>
2019-09-15 20:14:47 +1000

    Move the imports into __init__.py, and adjust import statements in individual files

--- project/builder.py
+++ project/builder.py
@@ -1,28 +1,21 @@
+from os import scandir
 
 from project.config import TEMPLATEDIR, IGNORED
-
-from project.baseproject import *
-from project.remoteproject import *
-
-from project.templates.akeneo import Akeneo
-from project.templates.pimcore import Pimcore
-from project.templates.drupal import Drupal7Vanilla, Drupal8, Drupal8multi, Opigno, Govcms8
-from project.templates.laravel import Laravel
-from project.templates.magento import Magento2ce
-from project.templates.rails import Rails
-from project.templates.sculpin import Sculpin
-from project.templates.symfony import Symfony3, Symfony4
-from project.templates.wordpress import Wordpress
+from project import BaseProject
 
 
 def project_factory(name):
-    '''Instantiate a project object.  Class selection is based on the following naming convention:
+    """
+    Instantiate a project object.  Class selection is based on the following naming convention:
     Project class matches template directory name with the first letter capitalized.
       laravel -> Laravel,
       drupal7_vanilla -> Drupal7_vanilla.
 
     The BaseProject class is used by default (class with the matching name is not imported)
-    '''
+
+    :param name: name of the project
+    :return: BaseProject
+    """
 
     targetclass = name.capitalize()
     try:
@@ -36,4 +29,4 @@ def project_list():
     Return a list of project factories defining the available projects
     :return: list
     """
-    return [project_factory(f.name) for f in os.scandir(TEMPLATEDIR) if f.is_dir() and f.name not in IGNORED]
+    return [project_factory(f.name) for f in scandir(TEMPLATEDIR) if f.is_dir() and f.name not in IGNORED]
